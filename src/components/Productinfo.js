e5cecc5c4873012ebb0f1440bd8321dd1e6d3d66
Visa <visalakshi.cse@gmail.com>
2020-04-24 23:34:12 -0500

    Formatted

--- src/components/Productinfo.js
+++ src/components/Productinfo.js
@@ -1,19 +1,17 @@
 
 import React,{Component} from 'react';
-import './Trending.scss'
+import './Style/Trending.scss'
 
 export default function Productinfo(props){
-    console.log(props.info[props.id])
+    
 return(
     <div>
     <div>
     {props.name[props.id]}
     </div>
-<div className="rendercomp">
-    
+    <div className="rendercomp">
     {props.info[props.id]}
-   
-    </div>
+     </div>
     </div>
 )
 }
